594c20a169f475c0809d0c9ac52d471c9299f9e7
Ethan Jones <joneetha@oregonstate.edu>
2019-11-29 18:47:50 -0800

    pass some tests

--- geo.py
+++ geo.py
@@ -4,10 +4,43 @@
 #
 #    def closest_segment(self, point):
 
+from collections import namedtuple
+
+NearestPoint = namedtuple('NearestPoint', 'point t')
+
+class Point:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+  def __add__(self, point):
+    return Point(self.x + point.x, self.y + point.y)
+
+  def __sub__(self, point):
+    return Point(self.x - point.x, self.y - point.y)
+
+  def dot(self, point):
+    return self.x * point.x + self.y * point.y
+
+  def scale(self, c):
+    return Point(self.x * c, self.y * c)
+
+  def distance_to(self, point):
+    dx = self.x - point.x
+    dy = self.y - point.y
+    return (dx * dx + dy * dy) ** 0.5
+
 class Segment:
   def __init__(self, start, end):
     self.start = start
     self.end = end
 
+  def nearest_point(self, point):
+    v = self.end - self.start
+    u = self.start - point
+    t = - (v.dot(u) / v.dot(v))
+    return NearestPoint(point = self.start + v.scale(t), t = t)
+
   def distance_to(self, point):
-    return 0
+    nearest = self.nearest_point(point)
+    return nearest.point.distance_to(point)
--- geotest.py
+++ geotest.py
@@ -1,29 +1,29 @@
-from geo import Segment
+from geo import Point, Segment
 import unittest
 
 class TestSegmentMethods(unittest.TestCase):
   def test_distance_to_start_is_zero(self):
-    s = Segment([0, 0], [1, 1])
+    s = Segment(Point(0, 0), Point(1, 1))
     
-    dist = s.distance_to([0, 0])
+    dist = s.distance_to(Point(0, 0))
     self.assertEqual(dist, 0)
 
   def test_distance_to_end_is_zero(self):
-    s = Segment([0, 0], [1, 1])
+    s = Segment(Point(0, 0), Point(1, 1))
 
-    dist = s.distance_to([1, 1])
+    dist = s.distance_to(Point(1, 1))
     self.assertEqual(dist, 0)
 
   def test_distance_to_1_0_is_sqrt_2_over_2(self):
-    s = Segment([0, 0], [1, 1])
+    s = Segment(Point(0, 0), Point(1, 1))
 
-    dist = s.distance_to([1, 0])
+    dist = s.distance_to(Point(1, 0))
     self.assertEqual(dist, 0.5 * (2.0 ** 0.5))
 
   def test_distance_to_2_2_is_sqrt_2(self):
-    s = Segment([0, 0], [1, 1])
+    s = Segment(Point(0, 0), Point(1, 1))
 
-    dist = s.distance_to([2, 2])
+    dist = s.distance_to(Point(2, 2))
     self.assertEqual(dist, 2.0 ** 0.5)
 
 if __name__ == '__main__':
