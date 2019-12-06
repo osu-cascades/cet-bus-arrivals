4d2db879272b0d2114509851896e99b00a83910e
Ethan Jones <joneetha@oregonstate.edu>
2019-12-06 08:28:24 -0800

    test guess_route

--- test_bus_routes.py
+++ test_bus_routes.py
@@ -2,7 +2,7 @@ import json
 import unittest
 
 from geo import Point, Segment, Polyline
-from bus_routes import enumerate_shapes
+from bus_routes import enumerate_shapes, guess_route
 
 class TestBusRoutesMethods(unittest.TestCase):
   def test_enumate_shapes_has_correct_keys(self):
@@ -70,6 +70,36 @@ class TestBusRoutesMethods(unittest.TestCase):
     """)
     with self.assertRaises(ValueError):
       enumerate_shapes(trips, shapes)
+
+  def test_guess_route(self):
+    trips_json = json.loads("""
+      [
+        {"shape_id": 0, "route_id": "route 1"},
+        {"shape_id": 1, "route_id": "route 2"}
+      ]
+    """)
+    shapes_json = json.loads("""
+      [
+        {"shape_pt_lat": 0, "shape_pt_lon": 0, "shape_id": 0},
+        {"shape_pt_lat": 0, "shape_pt_lon": 1, "shape_id": 0},
+        {"shape_pt_lat": 0, "shape_pt_lon": 2, "shape_id": 0},
+        {"shape_pt_lat": 0, "shape_pt_lon": 3, "shape_id": 0},
+
+        {"shape_pt_lat": 1, "shape_pt_lon": 0, "shape_id": 1},
+        {"shape_pt_lat": 1, "shape_pt_lon": 1, "shape_id": 1},
+        {"shape_pt_lat": 1, "shape_pt_lon": 2, "shape_id": 1},
+        {"shape_pt_lat": 1, "shape_pt_lon": 3, "shape_id": 1}
+      ]
+    """)
+    shapes = enumerate_shapes(trips_json, shapes_json)
+
+    bus = json.loads('{"latitude": 0.51, "longitude": 0}')
+    closest = guess_route(shapes, bus)
+    self.assertEqual(closest[1], "route 2")
+
+    bus = json.loads('{"latitude": 0.49, "longitude": 0}')
+    closest = guess_route(shapes, bus)
+    self.assertEqual(closest[1], "route 1")
 
 if __name__ == '__main__':
   unittest.main()
