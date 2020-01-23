cf95053c2209d1c0a6a8c212763da9843305f2f4
Devon Crane <SpartanPikmin@gmail.com>
2020-01-22 16:22:01 -0800

    Got colors for individual routes fixed.

--- static/app.js
+++ static/app.js
@@ -1,6 +1,53 @@
+let focusedRoute = null;
+let polylineLayer;
+let routes = new Map();
+
+let route_shapes = {
+  'route 290': ["p_30", "p_31", "p_746", "p_747", "p_748"],
+  'route 291': ["p_750","p_745","p_352","p_353"],
+  'route 292': ["p_749","p_1116"],
+  'route 293': ["p_1113","p_1112","p_744792","p_1667","p_1668"],
+  'route 3136': ["p_180304","p_176598"],
+  'route 3138': ["p_1105","p_176543"],
+  'route 4695': ["p_745174"],
+  'route 5917': ["p_1117","p_176608"],
+  'route 382': ["p_751","p_753","p_176606","p_176607"],
+  'route 710': ["p_1109","p_1124"],
+  'route 711': ["p_1106","p_1123"],
+  'route 712': ["p_1108","p_176539"],
+  'route 713': ["p_1121","p_8009"],
+  'route 714': ["p_1114","p_176595"],
+  'route 715': ["p_1110","p_176596"],
+  'route 716': ["p_177368","p_177368"],
+  'route 740': ["p_744877"],
+  'route 3225': ["p_180576","p_9617","p_180573","p_180574","p_111380"]
+};
+18
+let route_colors =[
+  'red',
+  'blue',
+  'green',
+  'yellow',
+  'pink',
+  'brown',
+  'teal',
+  'orange',
+  'purple',
+  'black',
+  '#00DCFF',
+  '#FF00AA',
+  '#31FF8F',
+  '#6FFF3',
+  '#31FFEC',
+  '#BD31FF',
+  '#FF31FF',
+  '#31FFA1'
+];
+
 document.addEventListener('DOMContentLoaded', function () {
   const mymap = L.map('mapid').setView([44.0583, -121.3154], 13);
   let buslayer = L.layerGroup().addTo(mymap);
+  polylineLayer = L.layerGroup().addTo(mymap);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -26,35 +73,41 @@ document.addEventListener('DOMContentLoaded', function () {
         L.marker([lat, lon]).bindPopup("<b>" + stop.stop_name + "</b><br>Your bus will arive in: " + stop.eta).addTo(mymap);
       }
     });
-  $.getJSON('/shape', function(data){
-    displayShape(mymap,["p_30", "p_31", "p_746", "p_747", "p_748"],data,'red','route 290');
-    displayShape(mymap,["p_750","p_745","p_352","p_353"],data,'blue','route 291');
-    displayShape(mymap,["p_749","p_1116"],data,'green','route 292');
-    displayShape(mymap,["p_1113","p_1112","p_744792","p_1667","p_1668"],data,'yellow','route 293');
-    displayShape(mymap,["p_180304","p_176598"],data,'grey','route 3136');
-    displayShape(mymap,["p_1105","p_176543"],data,'pink','route 3138');
-    displayShape(mymap,["p_180576","p_9617","p_180573","p_180574","p_111380"],data,'#31FF8F','route 3225');
-    displayShape(mymap,["p_751","p_753","p_176606","p_176607"],data,'#6FFF31','route 382');
-    displayShape(mymap,["p_745174"],data,'#31FFEC','route 4695');
-    displayShape(mymap,["p_1117","p_176608"],data,'#BD31FF','route 5917');
-    displayShape(mymap,["p_1109","p_1124"],data,'#FF31FF','route 710');
-    displayShape(mymap,["p_1106","p_1123"],data,'#FFCD31','route 711');
-    displayShape(mymap,["p_1108","p_176539"],data,'#31FFA1','route 712');
-    displayShape(mymap,["p_1121","p_8009"],data,'#31D3FF','route 713');
-    displayShape(mymap,["p_1114","p_176595"],data,'#8231FF','route 714');
-    displayShape(mymap,["p_1110","p_176596"],data,'#4431FF','route 715');
-    displayShape(mymap,["p_177368","p_177368"],data,'#F631FF','route 716');
-    displayShape(mymap,["p_744877"],data,'black','route 740');
-
+  $.getJSON('/shape', function(data) {
+    i = 0;
+    for (key in route_shapes) {
+      color = route_colors[i];
+      initializeRoutes(route_shapes[key], data, key);
+      drawRoute(mymap, key, color);
+      i++;
+    }
   });
   setInterval(() => displayData(mymap, buslayer, busicon), 1000);
 });
 
-function clickRoute(mymap, pointlist) {
-  mymap.fitBounds(pointlist);
+function clickRoute(mymap, route_id) {
+  let shapes = routes.get(route_id);
+  let verts = [];
+  for (shape of shapes.values()) {
+    verts = verts.concat(shape[1]);
+  }
+  mymap.fitBounds(verts);
 }
 
-function displayShape(mymap, shape_id_list,data,shape_color,route_id){
+function drawRoute(mymap, route_id, color) {
+  let pointlists = routes.get(route_id);
+  for (pointlist of pointlists) {
+    let polyline = new L.polyline(pointlist[1], {
+      color: color,
+      weight: 3,
+      opacity: 0.5,
+      smoothFactor: 1
+    });
+    polyline.bindPopup("<b>"+route_id+"</b>").on('click', e => clickRoute(mymap, route_id)).addTo(polylineLayer);
+  }
+}
+
+function initializeRoutes(shape_id_list, data, route_id){
   let polylines = new Map();
   for (let shape_id of shape_id_list) {
     polylines.set(shape_id, []);
@@ -65,16 +118,9 @@ function displayShape(mymap, shape_id_list,data,shape_color,route_id){
       polylines.get(shape_point.shape_id).push(point);
     }
   }
+  routes.set(route_id, []);
   for (let pointlist of polylines) {
-    let polyline = new L.polyline(pointlist[1], {
-        color: shape_color,
-        weight: 3,
-        opacity: 0.5,
-        smoothFactor: 1
-    });
-    polyline.bindPopup("<b>"+route_id+"</b>").on('click', e => {
-      clickRoute(mymap, pointlist[1]);
-    }).addTo(mymap);
+    routes.get(route_id).push(pointlist);
   }
 }
 
